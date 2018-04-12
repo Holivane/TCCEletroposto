43a118c52b4c5aed031a2dcb08e017bee5d8c81f
Marcio RibeiroMar <marciorcruz@hotmail.com>
2018-04-12 15:55:14 -0300

    Projeto Google Maps

---
+++ api/Controllers/TCCController.cs
@@ -0,0 +1,22 @@
+﻿using api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace api.Controllers
+{
+    [RoutePrefix("api/eletroposto")]
+    public class TCCController : ApiController
+    {
+        TCCELETROPOSTOEntities db = new TCCELETROPOSTOEntities();
+        
+        [AcceptVerbs("GET")]
+        [Route("getbus")]
+        public List<Barras> getbus()
+        {
+            return db.Barras.ToList();
+        }
+    }
+}
