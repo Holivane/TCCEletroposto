46d63680f23012ffeec8bd1a2768d228337e2e4b
Ananda Andrade Nascimento <ananda.nasc@hotmail.com>
2018-04-24 19:45:24 -0300

    Início da estrutura do site

---
+++ api/Controllers/AppController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace api.Controllers
+{
+    public class AppController : Controller
+    {
+        // GET: App
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Registre-se
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: Perfil
+        public ActionResult Perfil()
+        {
+            return View();
+        }
+
+        // GET: Alteração de Dados
+        public ActionResult AltDados()
+        {
+            return View();
+        }
+
+        // GET: Simulação
+        public ActionResult Simulacao()
+        {
+            return View();
+        }
+        
+        // GET: Sobre
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+    }
+}
