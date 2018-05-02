7b97d4fe3c3995117e27d84e3f2e216006d24476
Ananda <anandaan92@gmail.com>
2018-05-02 12:50:28 -0300

    ok

--- TesteEletroposto/Program.cs
+++ TesteEletroposto/Program.cs
@@ -21,69 +21,69 @@ namespace TesteEletroposto
             //Console.WriteLine("___________________________________________");
 
             //rede rede_id barra barra_id    vnom_kv utm_x   utm_y
-
+            BibliotecaOpenDSS.Uteis.Uteis.LerArquivo("");
 
        //Holivane 
-       Eletroposto lib = new Eletroposto("C:\\Users\\anand\\Documents\\TCCEletroposto\\TCCEletroposto\\ApiEletroposto\\Content\\Rede\\Sinap_Rede_CAI_teste_sem_trafos.dss");
+       //Eletroposto lib = new Eletroposto("C:\\Users\\anand\\Documents\\TCCEletroposto\\TCCEletroposto\\ApiEletroposto\\Content\\Rede\\Sinap_Rede_CAI_teste_sem_trafos.dss");
 
-            bool verificararquivoderede = lib.RunFile();
-
-
-            BibliotecaOpenDSS.Models.Carga carga = new Carga
-            {
-                Barra = "1115",
-                Nome = "CargaTeste",
-                PotenciaTotal = 50000
-            };
+            //     bool verificararquivoderede = lib.RunFile();
 
 
-            bool solve = lib.Solve();
-            //TRECHOS
-            List<Trecho> x = new List<Trecho>();
-            x = lib.TodosTrechos();
-
-            foreach (Trecho t in x)
-            {
-                if (t.barra2.CodBarra.Equals("1508"))
-                {
-                    Console.Out.WriteLine(t.CodTrecho + " --- " + t.IAtual);
-                }
-
-            }
-
-           
-
-            lib.AddLoad(carga);
-
-            solve = lib.Solve();
-
-            //TRECHOS
-            List<Trecho> xd = new List<Trecho>();
-            xd = lib.TodosTrechos();
-
-            foreach (Trecho t in xd)
-            {
-                if (t.barra2.CodBarra.Equals("1508"))
-                {
-                    Console.Out.WriteLine(t.CodTrecho + " --- " + t.IAtual);
-                }
-                
-
-            }
+            //     BibliotecaOpenDSS.Models.Carga carga = new Carga
+            //     {
+            //         Barra = "1115",
+            //         Nome = "CargaTeste",
+            //         PotenciaTotal = 50000
+            //     };
 
 
-            List<Barra> b = new List<Barra>();
-            b = lib.AllScore();
+            //     bool solve = lib.Solve();
+            //     //TRECHOS
+            //     List<Trecho> x = new List<Trecho>();
+            //     x = lib.TodosTrechos();
 
-            //TRECHOS
-            List<Carga> c = new List<Carga>();
-            c = lib.TodasCargas();
+            //     foreach (Trecho t in x)
+            //     {
+            //         if (t.barra2.CodBarra.Equals("1508"))
+            //         {
+            //             Console.Out.WriteLine(t.CodTrecho + " --- " + t.IAtual);
+            //         }
 
-            foreach (Carga t in c)
-            {
-                //Console.Out.WriteLine(t.Nome + " --- " + t.PotenciaTotal);
+            //     }
 
-            }
+
+
+            //     lib.AddLoad(carga);
+
+            //     solve = lib.Solve();
+
+            //     //TRECHOS
+            //     List<Trecho> xd = new List<Trecho>();
+            //     xd = lib.TodosTrechos();
+
+            //     foreach (Trecho t in xd)
+            //     {
+            //         if (t.barra2.CodBarra.Equals("1508"))
+            //         {
+            //             Console.Out.WriteLine(t.CodTrecho + " --- " + t.IAtual);
+            //         }
+
+
+            //     }
+
+
+            //     List<Barra> b = new List<Barra>();
+            //     b = lib.AllScore();
+
+            //     //TRECHOS
+            //     List<Carga> c = new List<Carga>();
+            //     c = lib.TodasCargas();
+
+            //     foreach (Carga t in c)
+            //     {
+            //         //Console.Out.WriteLine(t.Nome + " --- " + t.PotenciaTotal);
+
+            //     }
 
 
             Console.ReadLine();
--- api/Controllers/TCCController.cs
+++ api/Controllers/TCCController.cs
@@ -14,7 +14,7 @@ namespace api.Controllers
         TCCELETROPOSTOEntities db = new TCCELETROPOSTOEntities();
 
         //private string arquivoderede = @"D:\HOLIVANE\TCC\TCCEletroposto\Canindé\OpenDSS\Sinap_Rede_CAI_teste_sem_trafos.dss";
-       private string arquivoderede = HttpRuntime.AppDomainAppPath + "/Sinap_Rede_CAI_teste_sem_trafos_Ajustada2.dss";
+       private string arquivoderede = HttpRuntime.AppDomainAppPath + "/Sinap_Rede_CAI_teste_sem_trafos_vf.dss";
 
 
         [AcceptVerbs("GET")]
@@ -75,10 +75,10 @@ namespace api.Controllers
 
         [AcceptVerbs("GET")]
         [Route("updatebus")]
-        public string updatebus(string filename)
+        public string updatebus()
         {
             List<Barra> lista = new List<Barra>();
-            lista = BibliotecaOpenDSS.Uteis.Uteis.LerArquivo(filename);
+            lista = BibliotecaOpenDSS.Uteis.Uteis.LerArquivo("");
 
             // int count = 1;
            
